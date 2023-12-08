2673a3c2cd2398657727895e200e90ff32cd3883
madomaga <marcin.domagala@capgemini.com>
2023-12-08 08:44:55 +0100

    load model

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import * as tf from '@tensorflow/tfjs';
 import * as tfrn from '@tensorflow/tfjs-react-native';
 import { Link } from 'expo-router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ImagePickerComponent from '../../components/ImagePicker';
@@ -9,9 +9,22 @@ import ImagePickerComponent from '../../components/ImagePicker';
 export default function Page() {
   const [userImage, setUserImage] = useState();
 
+  useEffect(() => {
+    async function initializeTF() {
+      await tf.ready();
+    }
+
+    initializeTF();
+  }, []);
+
   async function loadModel() {
     const modelJSON = require('../../assets/model.json');
-    const modelWeights = await require('../../assets/group1-shard4of4.bin');
+    const modelWeights = await [
+      require('../../assets/group1-shard4of4.bin'),
+      require('../../assets/group1-shard3of4.bin'),
+      require('../../assets/group1-shard2of4.bin'),
+      require('../../assets/group1-shard1of4.bin'),
+    ];
 
     const model = await tf.loadLayersModel(
       tfrn.bundleResourceIO(modelJSON, modelWeights),
@@ -21,6 +34,7 @@ export default function Page() {
   }
 
   function preprocessImage(userImage: any) {
+    if(userImage !==)
     const tensor = tf.browser.fromPixels(userImage);
 
     return tensor;
