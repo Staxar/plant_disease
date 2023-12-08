9a4197a3382e96bed785c5d1861a635718fc1d06
madomaga <marcin.domagala@capgemini.com>
2023-12-08 15:40:30 +0100

    indicator

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,20 @@
 import * as tf from '@tensorflow/tfjs';
+import * as tfrn from '@tensorflow/tfjs-react-native';
 import { Link } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import {
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ImagePickerComponent from '../../components/ImagePicker';
 
 export default function Page() {
   const [userImage, setUserImage] = useState<string>('');
-
+  const [isPending, setIsPending] = useState<boolean>(false);
   useEffect(() => {
     async function initializeTF() {
       await tf.ready();
@@ -25,23 +32,35 @@ export default function Page() {
       require('../../assets/group1-shard1of4.bin'),
     ];
 
-    const model = await tf.loadLayersModel(modelJSON);
-
+    const model = await tf.loadLayersModel(
+      tfrn.bundleResourceIO(modelJSON, modelWeights),
+    );
+    console.log(model);
     return model;
   }
 
-  function preprocessImage(userImage: string) {
-    const tensor = tf.browser.fromPixels(userImage);
+  async function preprocessImage(userImage: string) {
+    const imageUri = 'https://cid-inc.com/app/uploads/2020/10/leaf_area.jpg';
+    const response = await tfrn.fetch(imageUri, {}, { isBinary: true });
+    const imageDataArrayBuffer = await response.arrayBuffer();
+    const imageData = new Uint8Array(imageDataArrayBuffer);
 
-    return tensor;
+    // Decode image data to a tensor
+    const imageTensor = tfrn
+      .decodeJpeg(imageData)
+      .resizeNearestNeighbor([224, 224]) // Resize to match the model's input dimensions
+      .expandDims(); // Add batch dimension
+
+    return imageTensor;
   }
 
   async function predict(userImage: string) {
     const model = await loadModel();
 
-    // const tensor = preprocessImage(userImage);
+    const tensor = await preprocessImage(userImage);
 
-    // const prediction = model.predict(tensor);
+    const prediction = model.predict(tensor);
+    console.log(prediction);
   }
 
   return (
@@ -49,6 +68,7 @@ export default function Page() {
       <View style={styles.main}>
         <Text style={styles.title}>Plant disease</Text>
         <ImagePickerComponent userImage={setUserImage} />
+        {isPending && <ActivityIndicator size="large" />}
         <View>
           <Text>Your plant is: </Text>
         </View>
